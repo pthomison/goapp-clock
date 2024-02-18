b61c412f6dc2e569555b008eef406846ab648c46
pthomison <p.thomison@gmail.com>
2024-02-18 12:49:51 -0800

    cmd updates

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+	"github.com/pthomison/errcheck"
+	"github.com/pthomison/goapp-clock/clock"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	addr = "127.0.0.1:8000"
+)
+
+func main() {
+	app.RouteFunc("/", func() app.Composer {
+		return &clock.Root{}
+	})
+	app.RunWhenOnBrowser()
+
+	// HTTP routing:
+	http.Handle("/", clock.App())
+
+	logrus.Info("Listening on ", addr)
+	err := http.ListenAndServe(addr, nil)
+	errcheck.Check(err)
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.21.3
 
 require (
 	github.com/maxence-charriere/go-app/v9 v9.8.0
+	github.com/pthomison/errcheck v0.0.3
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
-	github.com/google/uuid v1.4.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
